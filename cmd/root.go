87115334f90baed575af5afb786f6d9f8928516a
Ulises Fasoli <ufasoli@gmail.com>
2022-03-10 09:24:31 +0100

    add aarch64 support

--- cmd/root.go
+++ cmd/root.go
@@ -23,7 +23,7 @@ var Verbose bool
 
 const containerName = "platys"
 const configFilePath = "/opt/mdps-gen/vars/config.yml"
-const version = "2.4.3"
+const version = "2.4.4"
 
 var versionInfo = fmt.Sprintf(
 	`Platys - Trivadis Platform in a Box - v %v
